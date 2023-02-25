5b0b083722553a9b502ab7df571650f7627c29ee
James Smith <theTardigrade@users.noreply.github.com>
2023-02-25 14:37:21 +0000

    Add documentation comments for storage internal package

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -2,11 +2,16 @@ package storage
 
 import "time"
 
+// Datum is used to structure the information contained
+// in each of the files used in the cache.
 type Datum struct {
 	SetTime time.Time `json:"t"`
 	Content []byte    `json:"c"`
 }
 
+// NewDatum returns a pointer to a newly allocated
+// Datum struct, populating it with the given content
+// and the current time.
 func NewDatum(content []byte) *Datum {
 	return &Datum{
 		SetTime: time.Now().UTC(),
