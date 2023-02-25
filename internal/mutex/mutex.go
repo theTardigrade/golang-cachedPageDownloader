c79b280ae14f6bd2d60f40587b521c089fd39a3d
James Smith <theTardigrade@users.noreply.github.com>
2023-02-25 14:34:03 +0000

    Add documentation comments for mutex internal package

--- internal/mutex/mutex.go
+++ internal/mutex/mutex.go
@@ -30,6 +30,9 @@ func get(key string) (mutex *sync.Mutex) {
 	return mutex
 }
 
+// GetLocked returns a mutex from the collection,
+// based on the hash value of the given key,
+// after locking it.
 func GetLocked(key string) (mutex *sync.Mutex) {
 	mutex = get(key)
 
@@ -38,11 +41,19 @@ func GetLocked(key string) (mutex *sync.Mutex) {
 	return mutex
 }
 
-func GetUniqueLocked(primaryKey string, otherKeys ...string) (mutex *sync.Mutex, found bool) {
+// GetUniqueLocked attempts to returns a mutex
+// from the collection,
+// based on the hash value of the given primary key,
+// after locking it.
+// However, if the mutex found using the primary key
+// is identical to any of the mutexes found using any
+// of the secondary keys, then no mutex is returned
+// or locked.
+func GetUniqueLocked(primaryKey string, secondaryKeys ...string) (mutex *sync.Mutex, found bool) {
 	mutex = get(primaryKey)
 
-	for i := 0; i < len(otherKeys); i++ {
-		mutex2 := get(otherKeys[i])
+	for i := 0; i < len(secondaryKeys); i++ {
+		mutex2 := get(secondaryKeys[i])
 
 		if mutex == mutex2 {
 			return
