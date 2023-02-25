cf92467a64f35870c6e39ec7c1bf2b4513bf367c
James Smith <theTardigrade@users.noreply.github.com>
2023-02-25 20:03:57 +0000

    Create mutexFormatKeyPart function

--- downloader_mutexMethods.go
+++ downloader_mutexMethods.go
@@ -1,7 +1,6 @@
 package cachedPageDownloader
 
 import (
-	"fmt"
 	"strings"
 	"sync"
 
@@ -12,27 +11,45 @@ const (
 	mutexKeySeparator byte = '|'
 )
 
-func (downloader *Downloader) mutexKeyDefaultParts() []string {
-	return []string{
+func mutexFormatKeyPart(keyPart string) string {
+	keyPart = strings.ReplaceAll(
+		strings.TrimSpace(keyPart),
+		`\`,
+		`\\`,
+	)
+
+	keyPart = strings.ReplaceAll(
+		keyPart,
+		string(mutexKeySeparator),
+		`\`+string(mutexKeySeparator),
+	)
+
+	return keyPart
+}
+
+func (downloader *Downloader) mutexKeyDefaultParts() (keyParts []string) {
+	keyParts = []string{
 		downloader.options.CacheDir,
 	}
+
+	return
 }
 
 func (downloader *Downloader) mutexKey(keyParts []string) string {
 	var builder strings.Builder
 
 	if keyPartsLen := len(keyParts); keyPartsLen > 0 {
-		builder.WriteString(keyParts[0])
+		builder.WriteString(mutexFormatKeyPart(keyParts[0]))
 
 		for i := 1; i < keyPartsLen; i++ {
 			builder.WriteByte(mutexKeySeparator)
-			builder.WriteString(keyParts[i])
+			builder.WriteString(mutexFormatKeyPart(keyParts[i]))
 		}
 	}
 
 	for _, part := range downloader.mutexKeyDefaultParts() {
 		builder.WriteByte(mutexKeySeparator)
-		builder.WriteString(part)
+		builder.WriteString(mutexFormatKeyPart(part))
 	}
 
 	return builder.String()
@@ -40,7 +57,6 @@ func (downloader *Downloader) mutexKey(keyParts []string) string {
 
 func (downloader *Downloader) mutexLocked(primaryKeyParts ...string) (currentMutex *sync.Mutex) {
 	primaryKey := downloader.mutexKey(primaryKeyParts)
-	fmt.Println("KEY", primaryKey)
 	currentMutex = mutex.GetLocked(primaryKey)
 
 	return
